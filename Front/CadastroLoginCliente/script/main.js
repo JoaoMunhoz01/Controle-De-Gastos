ff778fca2c2844f25843e5199ddba4d6437db77a
JoaoMunhoz01 <joao_munhoz@ufms.br>
2021-12-22 13:37:11 -0400

    Adicionando criptografia na senha

--- Front/CadastroLoginCliente/script/main.js
+++ Front/CadastroLoginCliente/script/main.js
@@ -70,3 +70,21 @@ function addRow(tableID, val, val1, val2, val3) {
     newCell2.appendChild(newText2);
     newCell3.appendChild(newText3);
 }
+
+function Encrypt(value) 
+{
+  var result="";
+  for(i=0;i<value.length;i++)
+  {
+    if(i<value.length-1)
+    {
+        result+=value.charCodeAt(i)+10;
+        result+="-";
+    }
+    else
+    {
+        result+=value.charCodeAt(i)+10;
+    }
+  }
+  return result;
+}
