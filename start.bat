cda09926bf0ff239965081831823646c1dfc47c9
JoaoMunhoz01 <joao_munhoz@ufms.br>
2022-01-14 13:56:27 -0400

    Arquivo .bat para clonar o repositorio e startar os servidores

--- start.bat
+++ start.bat
@@ -1,17 +1,18 @@
 @echo off
 
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-
-choco upgrade chocolatey
-
-choco install --confirm git
-
 git clone https://github.com/JoaoMunhoz01/Controle-De-Gastos.git
 
 cd Controle-De-Gastos
 
 cd Api-Banco-Postgres
+
+set PGUSER=postgres
+set PGPASSWORD=postgres
+
+createdb -U postgres -h localhost -p 5432 controlegastos
+
 start /b startAPI.bat
+
 
 cd ..
 
